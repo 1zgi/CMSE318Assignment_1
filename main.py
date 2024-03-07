822c0965b1b29ae5b89e798da257f279a3db9aac
Izgi Kanatli <izgikanatli@gmail.com>
2024-03-08 00:33:53 +0300

    1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print("Welcome to Student Information System!\nFunctions:\n1-Add a New Student\t\t2-Find a Student\n"
       "3-Show All Students\t\t4-Show Students by Birth Date\n5-Delete a Student\t\t6-Enter Whole Data to File\n"
       "7-Modify Student Information\n8-Get the Data From .txt to Array\n9-Quit")
-choice = int(input("Type function's Number: "))
+choice=int(input("Type function's Number: "))
