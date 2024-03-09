94e3fb532170372e55c941427cd1bb5bc40ff30f
Ulaş <ulas.klvn@gmail.com>
2024-03-09 18:02:54 +0200

    Object used for students

--- file-op.py
+++ file-op.py
@@ -1,21 +1,17 @@
 from array import *
-std1=["22450287","Ulas","Kilivan","30.10.1999","male","Turkey"]
-std2=["17723499","Burak","Isaogullari","04.11.1999","male","Cyprus"]
-std3=["22450367","Izgi","Kanatli","05.07.2000","male","Cyprus"]
-std4=["22023255","Ahmet Eray","Alkan","05.04.2002","male","Turkey"]
-students=list()
-
+from Student import Student
+std1=Student("22450287","Ulas","Kilivan","30.10.1999","male","Turkey")
+std2=Student("17723499","Burak","Isaogullari","04.11.1999","male","Cyprus")
+std3=Student("22450367","Izgi","Kanatli","05.07.2000","male","Cyprus")
+std4=Student("22023255","Ahmet Eray","Alkan","05.04.2002","male","Turkey")
+#students=[std1,std2,std3,std4]
+students=[]
 def arrayToFile(students):
     stdFile=open("students.txt","a")#file opened in append mode
-    j=0
     stdString=""
     for student in students:
-        for i in student:#Converts list to string and adds "-" between datas
-            stdString+=student[j]+"-"
-            j += 1
-        stdString=stdString[:len(stdString)-1]+"%"#deletes unrelevant "-" sign and adds "%" sign for seperating students in file
-        j=0
-    stdString=stdString[:len(stdString)-1]#deletes unrelevant "&" sign
+        stdString+=student.get_student_number()+"-"+student.get_first_name()+"-"+student.get_last_name()+"-"+student.get_date_of_birth()+"-"+student.get_sex()+"-"+student.get_country_of_birth()+"%"
+    stdString=stdString[:len(stdString)-1]#deletes unrelevant "%" sign
     stdFile.write(stdString)
     stdFile.close()
 def readFromFile():
@@ -24,13 +20,15 @@ def readFromFile():
     tempStudents=stdString.split("%")#splits students from eachother
     for student in tempStudents:
         tmpList=student.split("-")#splits students datas from eachother
-        students.append(tmpList)
+        tmpStd=Student(tmpList[0],tmpList[1],tmpList[2],tmpList[3],tmpList[4],tmpList[5])
+        students.append(tmpStd)
+
 def deleteStudent(stdNum):
     readFromFile()
     j=0
     isNumExist=True#if number is not exist, second if condition will work
     for student in students:
-        if student[0]==stdNum:#checks entered value
+        if student.get_student_number()==stdNum:#checks entered value
             students.pop(j)
             isNumExist=False
         j += 1
