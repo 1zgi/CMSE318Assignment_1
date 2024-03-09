2937cd07139d1e0a4dcb49390225ebbfcc03eaf2
Izgi Kanatli <izgikanatli@gmail.com>
2024-03-09 18:45:29 +0300

    find student method added

--- StudentService.py
+++ StudentService.py
@@ -2,10 +2,15 @@ from Student import *
 
 
 class StudentService:
-    def __init__(self):
-        self.students = []
+    students_list = [100]
+    student = Student
 
     @staticmethod
     def add_student(self, student_number, first_name, last_name, date_of_birth, sex, country_of_birth):
         student = Student(student_number, first_name, last_name, date_of_birth, sex, country_of_birth)
         self.students.append(student)
+
+    def find_student_by_number(self, number):
+        for obj in self.student.get_student_number(number):
+            if number == self.student.get_student_number(number):
+                print(number)
