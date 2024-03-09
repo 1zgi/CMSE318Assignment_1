5f724f1780d47b00dfea8da6bedc6bd6e388cf91
Izgi Kanatli <izgikanatli@gmail.com>
2024-03-09 19:14:35 +0300

    find function updated

--- StudentService.py
+++ StudentService.py
@@ -1,16 +1,18 @@
 from Student import *
+from FileOp import *
 
 
 class StudentService:
     students_list = [100]
     student = Student
-
+    
     @staticmethod
     def add_student(self, student_number, first_name, last_name, date_of_birth, sex, country_of_birth):
         student = Student(student_number, first_name, last_name, date_of_birth, sex, country_of_birth)
         self.students.append(student)
+        FileOp.arrayToFile(self, student)
 
     def find_student_by_number(self, number):
-        for obj in self.student.get_student_number(number):
+        for student in self.student.get_student_number(number):
             if number == self.student.get_student_number(number):
-                print(number)
+                print(student.get_student_number())
